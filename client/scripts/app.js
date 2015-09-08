f6b0a2bd1a9a9c22c615abc736c1db66f8205a8a
stevenlundy <steven.lundy@gmail.com>
2015-09-08 10:00:46 -0700

    Updated client configuration to work with our server.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,6 +1,6 @@
 
 var app = {
-  server: 'https://api.parse.com/1/classes/chatterbox',
+  server: 'http://127.0.0.1:3000/classes/chatterbox',
   messages: {},
   chatBox: document.getElementById("chats"),
   room: null,
